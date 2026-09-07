e2baef594fdfdd84462aac1964a751b8615535d4
alfarajali <110019848+alfarajali@users.noreply.github.com>
2026-09-07 11:23:40 +0300

    Add files via upload

---
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,80 @@
+export default {
+  "bootstrapScriptContent": "import(\"/_next/static/chunks/index-D_V_rEFR.js\")",
+  "clientReferenceDeps": {
+    "066132fd2803": {
+      "js": [
+        "/_next/static/chunks/Icon-BFIcK819.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js",
+        "/_next/static/chunks/index-D_V_rEFR.js"
+      ],
+      "css": []
+    },
+    "877bf9fc4568": {
+      "js": [
+        "/_next/static/chunks/index-D_V_rEFR.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "4d0b646553ed": {
+      "js": [
+        "/_next/static/chunks/index-D_V_rEFR.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "8b58cbad025d": {
+      "js": [
+        "/_next/static/chunks/index-D_V_rEFR.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "249ade8191be": {
+      "js": [
+        "/_next/static/chunks/index-D_V_rEFR.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "f4fa97b4c255": {
+      "js": [
+        "/_next/static/chunks/layout-segment-context-BwxKY2aD.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/index-D_V_rEFR.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "416c5e021802": {
+      "js": [
+        "/_next/static/chunks/index-D_V_rEFR.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "25f07dc259d5": {
+      "js": [
+        "/_next/static/chunks/streamed-icons-Bumrcy-j.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js",
+        "/_next/static/chunks/index-D_V_rEFR.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/_next/static/css/index.Bzx183aW.css"
+      ]
+    }
+  }
+}
